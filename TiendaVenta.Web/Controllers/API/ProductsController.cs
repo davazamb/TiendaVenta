26bf00ab322838a68c455db1a5b22a7d7abf0e8a
David Ali Zambrano Burgos <davazamb10@outlook.com>
2019-04-21 12:06:25 -0400

    ProductViewModel y Publicado en Hosting sitio myAsp.Net

---
+++ TiendaVenta.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TiendaVenta.Web.Data;
+
+namespace TiendaVenta.Web.Controllers.API
+{
+	[Route("api/[Controller]")]
+	public class ProductsController : Controller
+	{
+		private readonly IProductRepository productRepository;
+
+		public ProductsController(IProductRepository productRepository)
+		{
+			this.productRepository = productRepository;
+		}
+
+		[HttpGet]
+		public IActionResult GetProducts()
+		{
+			return this.Ok(this.productRepository.GetAll());
+		}
+
+	}
+}
